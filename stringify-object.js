6767e0e547ae9850e8e01a2029b4b804ee5ffb61
Sindre Sorhus <sindresorhus@gmail.com>
2013-03-22 12:21:58 +0100

    Use `val` instead of `obj` as variable name

--- stringify-object.js
+++ stringify-object.js
@@ -2,21 +2,21 @@
 (function () {
 	'use strict';
 
-	function isObject(obj) {
-		return obj === Object(obj);
+	function isObject(val) {
+		return val === Object(val);
 	}
 
-	function isEmpty(obj) {
-		if (obj === null || obj === undefined) {
+	function isEmpty(val) {
+		if (val === undefined || val === null) {
 			return true;
 		}
 
-		if (Array.isArray(obj) || typeof obj === 'string') {
-			return obj.length === 0;
+		if (Array.isArray(val) || typeof val === 'string') {
+			return val.length === 0;
 		}
 
-		for (var key in obj) {
-			if (Object.prototype.hasOwnProperty.call(obj, key)) {
+		for (var key in val) {
+			if (Object.prototype.hasOwnProperty.call(val, key)) {
 				return false;
 			}
 		}
@@ -24,30 +24,30 @@
 		return true;
 	}
 
-	function stringifyObject (obj, opts, pad) {
+	function stringifyObject (val, opts, pad) {
 		var objKeys;
 		opts = opts || {};
 		opts.indent = opts.indent || '\t';
 		opts.quote = opts.singleQuotes === false ? '"' : '\'';
 		pad = pad || '';
 
-		if (Array.isArray(obj)) {
-			if (isEmpty(obj)) {
+		if (Array.isArray(val)) {
+			if (isEmpty(val)) {
 				return '[]';
 			}
 
-			return '[\n' + obj.map(function (el, i) {
-				var eol = obj.length - 1 === i ? '\n' : ',\n';
+			return '[\n' + val.map(function (el, i) {
+				var eol = val.length - 1 === i ? '\n' : ',\n';
 				return pad + opts.indent + stringifyObject(el, opts, pad + opts.indent) + eol;
 			}).join('') + pad + ']';
 		}
 
-		if (isObject(obj)) {
-			if (isEmpty(obj)) {
+		if (isObject(val)) {
+			if (isEmpty(val)) {
 				return '{}';
 			}
 
-			objKeys = Object.keys(obj);
+			objKeys = Object.keys(val);
 
 			return '{\n' + objKeys.map(function (el, i) {
 				var eol = objKeys.length - 1 === i ? '\n' : ',\n';
@@ -55,11 +55,11 @@
 				// the rest contains something other than `a-z0-9_`
 				// TODO: Find out why this don't work: `/^[^a-z_\$]|\W+/ig`
 				var key = /^[^a-z_]|\W+/ig.test(el) && el[0] !== '$' ? stringifyObject(el, opts) : el;
-				return pad + opts.indent + key + ': ' + stringifyObject(obj[el], opts, pad + opts.indent) + eol;
+				return pad + opts.indent + key + ': ' + stringifyObject(val[el], opts, pad + opts.indent) + eol;
 			}).join('') + pad + '}';
 		}
 
-		return opts.quote + obj.replace(
+		return opts.quote + val.replace(
 			opts.singleQuotes ? /'/g : /"/g,
 			opts.singleQuotes ? '\\\"' : '\\\''
 		) + opts.quote;
