ba7656360c7b98176dd22a8e231790e7abe3f9c3
Sindre Sorhus <sindresorhus@gmail.com>
2013-03-22 13:45:19 +0100

    Run tests in both Node and phantomjs
    Omg, so much extra work…

--- test/test-stringify-object.js
+++ test/test-stringify-object.js
@@ -1,13 +1,31 @@
 /*global describe, it */
 'use strict';
-var assert = require('assert');
-var fs = require('fs');
-var stringifyObject = require('../stringify-object');
+var isBrowser = typeof window !== 'undefined';
 
+if (isBrowser) {
+	var xhrSync = (function () {
+		/*global XMLHttpRequest */
+		var xhr = new XMLHttpRequest();
+		return function(method, url, callback) {
+			xhr.onreadystatechange = function () {
+				if (xhr.readyState === 4) {
+					callback(xhr.responseText);
+				}
+			};
+			xhr.open(method, url, false);
+			xhr.send();
+		};
+	})();
+} else {
+	var fs = require('fs');
+	var assert = require('chai').assert;
+	var stringifyObject = require('../stringify-object');
+}
 
 describe('stringifyObject()', function () {
 	/*jshint quotmark:false */
 	it('should stringify an object', function () {
+		var expected;
 		var obj = {
 			foo: "bar 'bar'",
 			foo2: [
@@ -29,7 +47,14 @@ describe('stringifyObject()', function () {
 			singleQuotes: false
 		});
 
-		var expected = fs.readFileSync('test/fixture/fixture.js', 'utf8');
+		if (isBrowser) {
+			xhrSync('get', 'fixture/fixture.js', function (data) {
+				expected = data;
+			});
+		} else {
+			expected = fs.readFileSync('test/fixture/fixture.js', 'utf8');
+		}
+
 		assert.equal(actual + '\n', expected);
 	});
 });
